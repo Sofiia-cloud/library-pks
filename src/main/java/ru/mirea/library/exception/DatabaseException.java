e537d629ac092c2c79122623f445a8ef126989b8
kazahd <tihonsemenov123@gmail.com>
2026-09-12 18:06:18 +0300

    fix(exception): restore DatabaseException class body

---
+++ src/main/java/ru/mirea/library/exception/DatabaseException.java
@@ -0,0 +1,10 @@
+package ru.mirea.library.exception;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
