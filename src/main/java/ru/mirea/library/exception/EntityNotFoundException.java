ba368acd4553859c29bd1df4a98e10509bfd56f1
kazahd <tihonsemenov123@gmail.com>
2026-09-12 13:12:54 +0300

    feat(exception): add EntityNotFoundException

---
+++ src/main/java/ru/mirea/library/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.mirea.library.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
